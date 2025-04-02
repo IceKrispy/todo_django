5597a9d141067b978425a372c37ee96c444a7cd8
IceKrispy <lunaan.godwin.29@gmail.com>
2025-04-03 01:39:47 +0800

    hahaaaayyyssssttt

--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from .models import TodoItem
 from .serializers import TodoItemSerializer
+from django.views.generic import TemplateView
 
 class TodoItemViewSet(viewsets.ModelViewSet):
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
